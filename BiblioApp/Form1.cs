882755d303b128161e56e36b5d8707e320799937
NelsonJfsg <nelson.estrella@neztersolutions.com>
2024-02-03 12:57:23 -0700

    init project

---
+++ BiblioApp/Form1.cs
@@ -0,0 +1,11 @@
+namespace BiblioApp {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            MessageBox.Show("Hi");
+        }
+    }
+}
